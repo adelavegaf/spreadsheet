92dc9968fedcc4668a38ae02985502b04b0d8308
Antonio de la Vega <antoniodlavga@gmail.com>
2020-08-15 00:02:00 +0100

    create tests for spreadsheet

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,31 @@
 struct Spreadsheet {
     grid: Vec<Vec<Value>>,
 }
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Value {
     Num(f64),
     Text(String),
     Formula(String),
+    Empty,
 }
 
 impl Spreadsheet {
-    fn set(&mut self, row: usize, col: usize, raw_val: String) {
-        self.grid[row][col] = parse_val(&raw_val);
+    fn new() -> Spreadsheet {
+        Spreadsheet {
+            grid: vec![vec![Value::Empty; 100]; 100],
+        }
+    }
+
+    fn set(&mut self, row: usize, col: usize, raw_val: &str) {
+        self.grid[row][col] = parse_val(raw_val);
     }
 
     fn eval(&self, row: usize, col: usize) -> String {
         match &self.grid[row][col] {
             Value::Num(n) => n.to_string(),
             Value::Text(s) => s.to_string(),
-            Value::Formula(_f) => "parse_formula".to_string(),
+            Value::Formula(f) => f.to_string(),
+            Value::Empty => "".to_string(),
         }
     }
 }
@@ -35,20 +43,46 @@ fn parse_val(input: &str) -> Value {
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn parse_val_returns_formula() {
-        assert_eq!(parse_val("=1+1"), Value::Formula("1+1".to_string()));
-    }
 
-    #[test]
-    fn parse_val_returns_num() {
-        assert_eq!(parse_val("1"), Value::Num(1.0));
-        assert_eq!(parse_val("1.78"), Value::Num(1.78));
-    }
+    mod spreadsheet {
+        use super::*;
+        #[test]
+        fn new_creates_empty_grid() {
+            let s = Spreadsheet::new();
+            for row in &s.grid {
+                for val in row {
+                    assert_eq!(val, &Value::Empty);
+                }
+            }
+            assert_eq!(s.grid.len(), 100);
+            assert_eq!(s.grid[0].len(), 100);
+        }
 
-    #[test]
-    fn parse_val_returns_text() {
-        assert_eq!(parse_val("1123.a"), Value::Text("1123.a".to_string()));
-        assert_eq!(parse_val(" ="), Value::Text(" =".to_string()));
+        #[test]
+        fn set_parses_formula() {
+            let mut s = Spreadsheet::new();
+            let row = 12;
+            let col = 33;
+            s.set(row, col, "=1+1");
+            assert_eq!(s.grid[row][col], Value::Formula("1+1".to_string()));
+        }
+
+        #[test]
+        fn set_parses_num() {
+            let mut s = Spreadsheet::new();
+            let row = 1;
+            let col = 44;
+            s.set(row, col, "1.78");
+            assert_eq!(s.grid[row][col], Value::Num(1.78));
+        }
+
+        #[test]
+        fn set_parses_text() {
+            let mut s = Spreadsheet::new();
+            let row = 1;
+            let col = 44;
+            s.set(row, col, "1.78a");
+            assert_eq!(s.grid[row][col], Value::Text("1.78a".to_string()));
+        }
     }
 }
