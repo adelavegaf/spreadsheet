39e65a7b55faf3331c558c4e2ec0a7ec79314aba
Antonio de la Vega <antoniodlavga@gmail.com>
2020-08-29 01:03:55 +0100

    Complete set implementation: parse + re-eval dependent cells

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,24 @@ Function ::= FnId '(' Range ')'
 Range ::= Reference '->' Reference
 Reference ::= '[' Number ',' Number ']'
 Number ::= Digit+
+
+TODO:
+- Improve error handling while parsing. Ideally, we would get "unexpected token in line x col y, found: w expected z"
+- Read how to lay out Enums that use Structs as values. Seems weird to share the name.
+- Rename Ref to something else since there's a ref primitive in rust. CellRef?
+- Test for parsers
+- Test for combinators
+- Make eval only calculate numbers, have separate method to return outbound refs.
 */
+use std::collections::HashSet;
+use std::mem;
 
 #[derive(Clone)]
 enum ExprTree {
     Empty,
+    // TODO: read about enum and structs and how to properly combine both
+    // Is it ok to have an enum value have the same name as the struct it refers
+    // to?
     Val(Value),
     Unary(Box<UnaryNode>),
     Binary(Box<BinaryNode>),
@@ -36,10 +49,19 @@ struct BinaryNode {
     right: ExprTree,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 enum Value {
     Num(f64),
+    // TODO: read about enum and structs and how to properly combine both
+    // Is it ok to have an enum value have the same name as the struct it refers
+    // to?
     Ref(usize, usize),
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+struct Ref {
+    row: usize,
+    col: usize,
 }
 
 struct Spreadsheet {
@@ -51,6 +73,8 @@ struct Cell {
     raw: String,
     expr: ExprTree,
     out: f64,
+    outbound_refs: HashSet<Ref>,
+    inbound_refs: HashSet<Ref>,
 }
 
 impl Default for Cell {
@@ -59,6 +83,8 @@ impl Default for Cell {
             raw: "".to_string(),
             expr: ExprTree::Empty,
             out: 0.,
+            outbound_refs: HashSet::new(),
+            inbound_refs: HashSet::new(),
         }
     }
 }
@@ -116,13 +142,42 @@ impl Spreadsheet {
             self.resize_rows(row * 2);
         }
 
+        let cur_ref = Ref { row, col };
+
+        // Replace old cell with a placeholder to deal with expired inbound references
+        let old_cell = mem::replace(&mut self.grid[row][col], Cell::new());
+        self.remove_inbound(cur_ref, &old_cell.outbound_refs);
+
+        // Create new cell
         let (expr, _) = cell(raw)?;
-        let out = eval(self, &expr);
-        let c = &mut self.grid[row][col];
-        c.raw = raw.to_string();
-        c.expr = expr;
-        c.out = out;
-        Ok(c)
+        let (out, outbound_refs) = eval(self, &expr);
+        let inbound_refs = old_cell.inbound_refs.clone();
+        let new_cell = Cell {
+            raw: raw.to_string(),
+            expr,
+            out,
+            outbound_refs,
+            inbound_refs,
+        };
+
+        // Replace placeholder with new cell and add new inbound references
+        self.add_inbound(cur_ref, &new_cell.outbound_refs);
+        self.grid[row][col] = new_cell;
+
+        if self.has_cycle(cur_ref) {
+            self.grid[row][col] = old_cell;
+            return Err("This cell introduces a cycle!");
+        }
+
+        // Our references form a DAG, we can toposort it to have the correct
+        // order we should re-eval our dependencies.
+        let eval_order = self.toposort_inbound(cur_ref);
+        for r in eval_order {
+            let (new_out, _) = eval(self, &self.grid[r.row][r.col].expr);
+            self.grid[r.row][r.col].out = new_out;
+        }
+
+        Ok(&self.grid[row][col])
     }
 
     fn resize_rows(&mut self, new_len: usize) {
@@ -135,16 +190,76 @@ impl Spreadsheet {
             row.resize(new_len, Cell::new());
         }
     }
+
+    fn remove_inbound(&mut self, r: Ref, targets: &HashSet<Ref>) {
+        for t in targets.iter() {
+            self.grid[t.row][t.col].inbound_refs.remove(&r);
+        }
+    }
+
+    fn add_inbound(&mut self, r: Ref, targets: &HashSet<Ref>) {
+        for t in targets.iter() {
+            self.grid[t.row][t.col].inbound_refs.insert(r);
+        }
+    }
+
+    fn has_cycle(&self, start: Ref) -> bool {
+        self.recursive_has_cycle(start, &mut HashSet::new())
+    }
+
+    fn recursive_has_cycle(&self, start: Ref, visited: &mut HashSet<Ref>) -> bool {
+        if !visited.insert(start) {
+            return true;
+        }
+        for r in self.grid[start.row][start.col].outbound_refs.iter() {
+            if self.recursive_has_cycle(*r, visited) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn toposort_inbound(&self, start: Ref) -> Vec<Ref> {
+        let mut sorted = vec![];
+        self.recursive_toposort_inbound(start, &mut sorted);
+        sorted
+    }
+
+    fn recursive_toposort_inbound(&self, start: Ref, result: &mut Vec<Ref>) {
+        result.push(start);
+        for r in self.grid[start.row][start.col].inbound_refs.iter() {
+            self.recursive_toposort_inbound(*r, result);
+        }
+    }
 }
 
-fn eval(ss: &Spreadsheet, tree: &ExprTree) -> f64 {
+// TODO(adelavega): not a fan of returning both things here
+// Make eval only calculate a number, and bite the performance cost
+// and find references in separate method.
+fn eval(ss: &Spreadsheet, tree: &ExprTree) -> (f64, HashSet<Ref>) {
+    let mut refs = HashSet::new();
+    let out = recursive_eval(ss, tree, &mut refs);
+    (out, refs)
+}
+
+fn recursive_eval(ss: &Spreadsheet, tree: &ExprTree, refs: &mut HashSet<Ref>) -> f64 {
     match tree {
         ExprTree::Val(val) => match val {
             Value::Num(n) => *n,
-            Value::Ref(i, j) => ss.grid[*i][*j].out,
+            Value::Ref(row, col) => {
+                refs.insert(Ref {
+                    row: *row,
+                    col: *col,
+                });
+                // TODO: this might be out of bounds. Better to call a method to get?
+                ss.grid[*row][*col].out
+            }
         },
-        ExprTree::Unary(u) => u.op.apply(eval(ss, &u.child)),
-        ExprTree::Binary(b) => b.op.apply(eval(ss, &b.left), eval(ss, &b.right)),
+        ExprTree::Unary(u) => u.op.apply(recursive_eval(ss, &u.child, refs)),
+        ExprTree::Binary(b) => b.op.apply(
+            recursive_eval(ss, &b.left, refs),
+            recursive_eval(ss, &b.right, refs),
+        ),
         ExprTree::Empty => panic!("Found empty tree node"),
     }
 }
@@ -398,7 +513,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn set_formula_with_numbers() {
+    fn set_evals_formula_with_numbers() {
         let mut ss = Spreadsheet::new();
         let c1 = ss.set(0, 0, "=1+2*10-2").unwrap();
         assert_eq!(c1.out, 19.);
@@ -408,7 +523,7 @@ mod tests {
     }
 
     #[test]
-    fn set_formula_with_ref() {
+    fn set_evals_formula_with_ref() {
         let mut ss = Spreadsheet::new();
         ss.set(0, 0, "1").unwrap();
         ss.set(0, 1, "2").unwrap();
@@ -416,5 +531,51 @@ mod tests {
         ss.set(1, 1, "4").unwrap();
         let c = ss.set(2, 2, "=[0,0]+[0,1]+[1,0]+[1,1]").unwrap();
         assert_eq!(c.out, 10.);
+    }
+
+    #[test]
+    fn set_detects_ref_cycle() {
+        let mut ss = Spreadsheet::new();
+        ss.set(0, 0, "=[0,1]").unwrap();
+        ss.set(0, 1, "=[1,0]").unwrap();
+        assert!(ss.set(1, 0, "=[0,0]").is_err())
+    }
+
+    #[test]
+    fn set_evals_all_inbound() {
+        let mut ss = Spreadsheet::new();
+        ss.set(0, 0, "10").unwrap();
+        ss.set(0, 1, "=[0,0]*2").unwrap();
+        ss.set(1, 0, "=[0,0]*3").unwrap();
+        ss.set(1, 1, "=[1,0]*4").unwrap();
+
+        ss.set(0, 0, "1").unwrap();
+        assert_eq!(ss.grid[0][0].out, 1.);
+        assert_eq!(ss.grid[0][1].out, 2.);
+        assert_eq!(ss.grid[1][0].out, 3.);
+        assert_eq!(ss.grid[1][1].out, 12.);
+    }
+
+    #[test]
+    fn set_updates_inbound_outbound_refs() {
+        let mut ss = Spreadsheet::new();
+        ss.set(0, 0, "=[0,1]").unwrap();
+        ss.set(1, 1, "=[0,0]").unwrap();
+
+        ss.set(0, 0, "1").unwrap();
+        assert_eq!(
+            ss.grid[0][0].inbound_refs,
+            vec![Ref { row: 1, col: 1 }].into_iter().collect()
+        );
+        assert_eq!(ss.grid[0][0].outbound_refs, HashSet::new());
+
+        assert_eq!(ss.grid[0][1].inbound_refs, HashSet::new());
+        assert_eq!(ss.grid[0][1].outbound_refs, HashSet::new());
+
+        assert_eq!(ss.grid[1][1].inbound_refs, HashSet::new());
+        assert_eq!(
+            ss.grid[1][1].outbound_refs,
+            vec![Ref { row: 0, col: 0 }].into_iter().collect()
+        );
     }
 }
